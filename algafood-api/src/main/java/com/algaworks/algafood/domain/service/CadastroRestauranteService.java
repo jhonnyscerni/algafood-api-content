83197c464ee468585d43b582e070f8c48e90af5b
jhonnyscerni <jhonnyscerni@gmail.com>
2019-10-15 10:22:26 -0300

    refs #0007-4- refatorando-os-servicos-rest

--- algafood-api/src/main/java/com/algaworks/algafood/domain/service/CadastroRestauranteService.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/service/CadastroRestauranteService.java
@@ -6,28 +6,38 @@ import org.springframework.stereotype.Service;
 import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.model.Restaurante;
-import com.algaworks.algafood.domain.repository.CozinhaRepository;
 import com.algaworks.algafood.domain.repository.RestauranteRepository;
 
 @Service
 public class CadastroRestauranteService {
 
+	private static final String MSG_RESTAURANTE_NAO_ENCONTRADO 
+		= "Não existe um cadastro de restaurante com código %d";
+	
 	@Autowired
 	private RestauranteRepository restauranteRepository;
 	
 	@Autowired
-	private CozinhaRepository cozinhaRepository;
+	private CadastroCozinhaService cadastroCozinha;
 	
 	public Restaurante salvar(Restaurante restaurante) {
 		Long cozinhaId = restaurante.getCozinha().getId();
 		
-		Cozinha cozinha = cozinhaRepository.findById(cozinhaId)
-			.orElseThrow(() -> new EntidadeNaoEncontradaException(
-					String.format("Não existe cadastro de cozinha com código %d", cozinhaId)));
+		Cozinha cozinha = cadastroCozinha.buscarOuFalhar(cozinhaId);
+		
+//		Cozinha cozinha = cozinhaRepository.findById(cozinhaId)
+//			.orElseThrow(() -> new EntidadeNaoEncontradaException(
+//					String.format("Não existe cadastro de cozinha com código %d", cozinhaId)));
 		
 		restaurante.setCozinha(cozinha);
 		
 		return restauranteRepository.save(restaurante);
+	}
+	
+	public Restaurante buscarOuFalhar(Long cidadeId) {
+		return restauranteRepository.findById(cidadeId)
+			.orElseThrow(() -> new EntidadeNaoEncontradaException(
+					String.format(MSG_RESTAURANTE_NAO_ENCONTRADO, cidadeId)));
 	}
 	
 }
